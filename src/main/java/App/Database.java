09064d389ef81c7f988170ee95cde1d0dea187a8
nhatvan1912 <112192624+nhatvan1912@users.noreply.github.com>
2024-10-11 10:29:48 +0700

    mainInterface

--- src/main/java/App/Database.java
+++ src/main/java/App/Database.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class Database {
     private final static String url = "jdbc:mysql://localhost:3306/storemanager";
     private final static String username = "root";
-    private final static String password = "";
+    private final static String password = "12345678";
     public static Connection connectDB(){
         try{
             Connection connect = DriverManager.getConnection(url, username, password);
